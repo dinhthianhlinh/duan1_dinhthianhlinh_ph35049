d5c2f55684fa11c9bfff866603a0f875e6c9c878
dinhthianhlinh <127111321+dinhthianhlinh@users.noreply.github.com>
2023-12-14 23:24:34 +0700

    dinhlinh

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DuAn1_DINHTHIANHLINH_PH35049"
+include(":app")
+ 
